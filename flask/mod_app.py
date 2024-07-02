15c4fdfa5976a7e813b2b1f2f2f5d686cfc90511
Anirud007 <anirudrathy28@gmail.com>
2024-07-03 02:51:58 +0530

    fix frontend

--- flask/mod_app.py
+++ flask/mod_app.py
@@ -3,16 +3,25 @@ from flask import Flask, render_template, url_for, redirect, jsonify, request
 app = Flask(__name__)
 
 crew_list = {}
+agents = {}
 crew_counter = 0
+agent_counter = 0
 
+# Frontend routers
 @app.route('/')
 def index():
     return render_template('mod_in.html')
 
 @app.route('/page')
 def mod_ind():
-    return render_template('page.html')
+    crew_id = request.args.get('crew_id')
+    return render_template('page.html', crew_id=crew_id)
 
+@app.route('/page/<crew_id>/add_agent')
+def add_agent(crew_id):
+    return render_template('form_page.html', crew_id=crew_id)
+
+# Crew endpoints
 @app.route('/crews', methods = ['GET'])
 def get_all_crew():
     return jsonify(crew_list)
@@ -33,14 +42,20 @@ def make_crew():
         content = request.get_json()
 
         crew_name = content.get('name')
-        verbose = content.get('verbose', False)
-        memorisation = content.get('memorisation', False)
+        verbose = content.get('verbose')
+        memorisation = content.get('memorisation')
         full_id = 'crew'+str(crew_counter)
+
+        if bool(memorisation) == True:
+            memorisation = True
+        else:
+            memorisation = False
         
         crew_list[full_id] = {
             "name" : crew_name,
             "verbose" : verbose,
-            "memorisation": memorisation
+            "memorisation": memorisation,
+            "agents" : []
         }
         crew_counter += 1
 
@@ -55,6 +70,60 @@ def delete_instance(crew_id):
     
     del crew_list[crew_id]
     return jsonify({"message": "Crew deleted"}), 200
+
+# Agents routes
+
+@app.route('/crews/<crew_id>/agents', methods=['POST'])
+def create_instance(crew_id):
+    global agent_counter
+
+    try:
+        # content = request.get_json()
+        if request.method == 'POST':
+
+            role = request.form.get('name')
+            backstory = request.form.get('backstory')
+            goals = request.form.get('goals')
+            task = request.form.get('task')
+            tools = request.form.get('tools')
+            # exp_output = request.form.get('exp_output')
+            # allow_delegation = request.form.get('allow_delegation')
+            
+            # if bool(allow_delegation) != True:
+            #     allow_delegation = False
+            # else:
+            #     allow_delegation = True
+
+            full_id = 'agent'+ str(agent_counter)
+            task_id = 'task' + str(agent_counter)
+
+            # automater = Ai_Model(role, backstory, goal, task, exp_output, allow_delegation)
+            # agent, tasks = automater.make_agent(full_id, task_id)
+            
+            # agent_list.append(agent)
+            # task_list.append(tasks)
+            # print(agent_list,'\n',task_list)
+
+            # "exp_output": exp_output,
+            # "allow_delegation" : allow_delegation
+
+            agents[full_id] = {
+                "id": full_id,
+                "role": role,
+                "backstory": backstory,
+                "goals": goals,
+                "task": task,
+                "tools": tools
+            }
+            agent_counter+=1
+
+            if crew_id not in crew_list:
+                return jsonify({"Error" : "Crew Not Found"}) , 404
+            crew_list[crew_id]["agents"].append(agents[full_id])
+    except Exception as e:
+        return jsonify({"error" : f"{e}"}), 400
+
+    return redirect(url_for('mod_ind'))
     
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flask/static/js/page.js
@@ -0,0 +1,56 @@
+async function fetch_crews() {
+    const response = await fetch('/crews');
+    const crws = await response.json();
+    const crewDivlist = document.getElementById('crewDivlist');
+    crewDivlist.innerHTML = '';
+
+    for (const [id, crw] of Object.entries(crws)) {
+
+        const instanceDiv = document.createElement('div');
+        instanceDiv.className = 'container bg-success bg-opacity-50 py-2 px-4 my-3 border border-success border-2 rounded-3 shadow';
+
+        const ins_subDiv = document.createElement('div');
+        ins_subDiv.className = 'd-flex flex-row justify-content-center';
+
+        const text_div = document.createElement('div');
+        text_div.className = 'w-100 py-2';
+
+        const Agent_name = document.createElement('strong')
+        Agent_name.textContent = 'Crew Name : '
+
+        const instanceText = document.createElement('p');
+        instanceText.textContent = `${crw.name}`;
+
+        const del_btn_div = document.createElement('div');
+        del_btn_div.className = 'flex-shrink-0'
+
+        const addButton = document.createElement('a');
+        addButton.textContent = 'Add Agent'
+        addButton.className = 'btn btn-primary my-1';
+        addButton.href = `/page/${id}/add_agent`;
+
+        
+        
+        text_div.appendChild(Agent_name);
+        text_div.appendChild(instanceText);
+        del_btn_div.appendChild(addButton);
+        ins_subDiv.appendChild(text_div);
+        ins_subDiv.appendChild(del_btn_div);
+        instanceDiv.appendChild(ins_subDiv);
+
+        crw.agents.forEach(agent => {
+            const agentsDiv = document.createElement('div');
+            agentsDiv.className = 'container';
+            const agentText = document.createElement('p');
+            agentText.textContent = "Agent -- ";
+            agentText.textContent += ` ${agent.role}\n`;
+            agentsDiv.appendChild(agentText);
+            instanceDiv.appendChild(agentsDiv);
+        });
+
+        crewDivlist.appendChild(instanceDiv);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetch_crews);
+
